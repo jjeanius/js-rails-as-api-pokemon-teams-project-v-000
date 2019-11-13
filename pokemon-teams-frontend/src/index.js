86255635429e8446c706cd90ca20802824647b3b
judy <jjeanius@verizon.net>
2019-11-12 23:54:52 -0500

    addEventListener

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,16 @@
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    const BASE_URL = "http://localhost:3000"
+    const TRAINERS_URL = `${BASE_URL}/trainers`
+    const POKEMONS_URL = `${BASE_URL}/pokemons`
+    let button = document.querySelector('button');
+
+button.addEventListener('click', addPokemon) // allow us to listen to load the content and execute call back function
+    function addPokemon(ev){
+        console.log(ev.type, ev.target)
+    };
+
+
+}
