15ced944c30b9de08c1944d176277af2c21d2f76
judy <jjeanius@verizon.net>
2019-12-30 01:35:50 -0500

    adding frontend codes

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -7,46 +7,62 @@
     const POKEMONS_URL = `${BASE_URL}/pokemons`
     const mainDiv = document.querySelector('main')
 
-       fetch (TRAINERS_URL) // fetch request is a get request returns a promise containing
+       fetch(TRAINERS_URL) // fetch request is a get request returns a promise containing
         .then(res => res.json()) // a response object, which parse the response into json using.then
-        .then(addTrainers)  //we need to take our parsed data and feed it inot a function called addTrainers
+        .then(addTrainers)  //we need to take our parsed data and feed it into a function called addTrainers
 
         function addTrainers(trainers) {
            trainers.forEach(trainer => {
-               let pokiString = " "
+               let pokiString = ""
                trainer.pokemons.forEach(pokemon => {
-                   pokiString +=
-                   `<li> ${pokemon.nickname} (${pokemon.species})
-                   <button class= "release" data-pokemon-id = "${pokemon.id}"> Release</button> </li>`
+                   pokiString += `<li> ${pokemon.nickname} (${pokemon.species})
+                   <button class="release" data-pokemon-id="${pokemon.id}">Release</button> </li>`
                })
 
-               mainDiv.innerHTML +=  // adding to the innerHTML
+               mainDiv.innerHTML +=  // adding to the HTML
                `<div class="card" data-id="${trainer.id}"> <p> ${trainer.name} </p>
-               <button data-trainer-id ="${trainer_id}" > Add Pokemon </button>
-               <ul> ${pokiString} </ul></div>`
+               <button data-trainer-id ="${trainer.id}" > Add Pokemon </button>
+               <ul> ${pokiString} </ul>
+               </div>`
             })
-        }
 
         mainDiv.addEventListener('click', e=> {   // adding EventListener to DOM
-            debugger
-        }
-
             if (e.target.dataset.trainerId !== undefined) {
                 fetch(POKEMONS_URL, {
                     method: "POST",
                     headers: {
                         'Content-Type': 'applicaiton/json'
                     },
-                    body:JSON.stringify({
+                    body: JSON.stringify({
                         trainer_id: e.target.dataset.trainerId
                     })
                 })
                 .then(res =>res.json())
                 .then(addPokemon)
-            }
-        
+                }
 
-        function addPokemon(pokemon) {
+
+        function addPokemon(pokemon){
             mainDiv.children[pokemon.trainer_id-1].lastElementChild.innerHTML +=
-            `<li> ${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}"> Release </button> </button></li>`
+            `<li> ${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}"> Release </button></li>`
         }
+
+        mainDiv.addEventListener('click', e =>{
+            if(e.target.dataset.trainerId !== undefined) {
+                fetch(POKEMONS_URL, {
+                    method: "POST",
+                    headers: {
+                        'Content_Type': 'application/json'
+                    },
+                    body: JSON.stringify ({
+                        trainer_id: e.target.dataset.trainerId
+                    })
+                })
+                .then(res => res.json())
+                .then(addPokemon)
+                }
+                if (e.target.dataset.pokemonId !==undefined){
+                    e.target.parentElement.remove()
+                    fetch(POKEMONS_URL + '/' + e.target.dataset.pokemonId, {method: 'DELETE'})
+                   })
+                }
